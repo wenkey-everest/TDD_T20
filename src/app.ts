c4a425f7f4aeff23c32938a572f61b3378b0ab69
wenkey <muni.v.g@everest.engineering>
2021-10-11 16:12:28 +0530

    code for total_balls is added

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("inital setup...")
+let oneBall:number;
+let oneOver:number;
+let total_balls:number=0;
+function ballsRemaining(){
+    for( oneOver=0; oneOver<=3;oneOver++){
+        for( oneBall=1;oneBall<=6;oneBall++){
+            total_balls++;
+}
+    }
+    return total_balls;
+}
+
+export{ballsRemaining};
