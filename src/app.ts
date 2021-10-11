fb90fed782fd512182646a42324eaad44b704fe2
wenkey <muni.v.g@everest.engineering>
2021-10-11 17:22:14 +0530

    random number code added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 let oneBall:number;
 let oneOver:number;
 let total_balls:number=0;
+
+
+function randTest(spec:any):any {
+    var i, sum=0, r=Math.random();
+    for (i in spec) {
+      sum += spec[i];
+      if (r <= sum) return i;
+    }
+  }  
+
 function ballsRemaining(){
     for( oneOver=0; oneOver<=3;oneOver++){
         for( oneBall=1;oneBall<=6;oneBall++){
@@ -11,3 +21,4 @@ function ballsRemaining(){
 }
 
 export{ballsRemaining};
+export{randTest};
