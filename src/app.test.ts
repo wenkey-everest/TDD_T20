6f85fe5f61d718cb006998cebd9b9b8bf14b2177
wenkey <muni.v.g@everest.engineering>
2021-10-12 18:22:06 +0530

    strike change testing updated

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,11 +1,10 @@
-import { createIf } from "typescript";
-import {ballsRemaining, randTest} from "./app"
+import {ballsRemaining, randTest, strikeChangingWithScore, strikeChangingWithOver} from "./app"
+let playerNames:string[]=["kirat Boli", "N.s Nodhi", "R Rumrah", "Shashi Henra"];
 let probablity:Array<any> = [
     {"Dot":.05,1:.3, 2:.25, 3:.1, 4:.15, 5:.01, 6:.09,"out":.05},
-{"Dot":.1,1:.4, 2:.2, 3:.05, 4:.1, 5:.01, 6:.04,"out":.1},
-{"Dot":.2,1:.3, 2:.15, 3:.05, 4:.05, 5:.01, 6:.04,"out":.2},
-{"Dot":.3,1:.25, 2:.05, 3:0, 4:.05, 5:.01, 6:.04,"out":.3}];
-
+    {"Dot":.1,1:.4, 2:.2, 3:.05, 4:.1, 5:.01, 6:.04,"out":.1},
+    {"Dot":.2,1:.3, 2:.15, 3:.05, 4:.05, 5:.01, 6:.04,"out":.2},
+    {"Dot":.3,1:.25, 2:.05, 3:0, 4:.05, 5:.01, 6:.04,"out":.3}];
 
 test("test for number of balls left is 24", ()=>{
     const totalBall=ballsRemaining();
@@ -14,20 +13,23 @@ test("test for number of balls left is 24", ()=>{
 
 test("testing of random numbers for score call",()=>{
     let i:number =Math.floor(Math.random()*probablity.length);
-        console.log(i);
         let temp = randTest(probablity[i]);
         if(temp=="Dot"){
-            console.log(temp);
             expect(temp).toEqual("Dot");
         }else if(temp=="out"){
-            console.log(temp);
             expect(temp).toEqual("out");
         }else{
-            console.log(temp);
             expect(parseInt(temp)).toBeLessThanOrEqual(6);
             expect(parseInt(temp)).toBeGreaterThanOrEqual(1);
         }
-
-    }
-  
-)
+    })
+test("changing the strike is failed in run case",()=>{
+         const score:number=ballsRemaining();
+         const scoreStrike:string = strikeChangingWithScore(score);  
+         expect(playerNames).toContain(scoreStrike);
+    
+})
+test("changing of strike is failed in over case",()=>{
+        const strikeOver=strikeChangingWithOver();
+        expect(playerNames).toContain(strikeOver);
+});
